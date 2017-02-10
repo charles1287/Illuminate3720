a7de441fa814551d8da7355079136107bdf616b0
tiboedturtels <u0927597@utah.edu>
2017-02-09 17:42:26 -0700

    Set up a moving box

---
+++ ProjectCardinalPioneers/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    const float _speed = 3f;
+
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = _speed * new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	}
+}
