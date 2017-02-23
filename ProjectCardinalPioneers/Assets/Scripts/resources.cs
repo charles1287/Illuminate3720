3c101b1e3c552162c5bcbce53c54c914794caf4b
Unknown <charlesarvie2@gmail.com>
2017-02-23 16:19:17 -0700

    Resources
    Science'd the shit out of it

---
+++ ProjectCardinalPioneers/Assets/Scripts/resources.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resources : MonoBehaviour {
+    public float water = 2000.0f;
+    public float energy = 2000.0f;
+    public int potato = 50000;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
