efd4385d931a63a994b0510fc7fdeb6e73bb5861
tiboedturtels <u0927597@utah.edu>
2017-02-23 15:47:20 -0700

    Got wire to show up

--- ProjectCardinalPioneers/Assets/Scripts/PlayerController.cs
+++ ProjectCardinalPioneers/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,17 @@ public class PlayerController : MonoBehaviour {
     const float _speed = 3f;
     Vector2 _direction = Vector2.down;
 
-    public Rigidbody2D rb;
+    Rigidbody2D _rb;
+    DistanceJoint2D _hook;
+    LineRenderer _wire;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _hook = GetComponent<DistanceJoint2D>();
+        _wire = GetComponent<LineRenderer>();
+    }
+    
 	// Update is called once per frame
 	void Update () {
         #region Movement Input
@@ -23,7 +27,7 @@ public class PlayerController : MonoBehaviour {
         //changes direction if necessary
         if(input != Vector2.zero)
             _direction = input.normalized;
-        rb.velocity = _speed * input.normalized;
+        _rb.velocity = _speed * input.normalized;
         #endregion
 
         #region Interact Input
@@ -36,17 +40,39 @@ public class PlayerController : MonoBehaviour {
 
         if(Input.GetMouseButtonDown(0))
         {
-            Vector3 ray =  Camera.main.ScreenToWorldPoint((Input.mousePosition)) - transform.position;
-            Debug.DrawRay(transform.position, ray);
-            
-            RaycastHit2D hitInfo = Physics2D.Linecast(transform.position, ray, LayerMask.GetMask("Draggable"));
-            if (hitInfo)
-            {
-                DistanceJoint2D joint = hitInfo.rigidbody.GetComponent<DistanceJoint2D>();
-                joint.anchor = hitInfo.transform.InverseTransformPoint(hitInfo.point);
-                joint.connectedBody = rb;
-            }
+            _hook.enabled = false;
+            _wire.enabled = false;
+            FireHook();
         }
         #endregion
+
+        UpdateWire();
     }
+
+    //Casts a line and hooks up the DistanceJoint2D if necessary
+    void FireHook()
+    {
+        Vector3 ray = Camera.main.ScreenToWorldPoint((Input.mousePosition)) - transform.position;
+        Debug.DrawRay(transform.position, ray);
+
+        RaycastHit2D hitInfo = Physics2D.Linecast(transform.position, ray, LayerMask.GetMask("Draggable"));
+        if (hitInfo)
+        {
+            _hook.enabled = true;
+            _wire.enabled = true;
+            _hook.connectedAnchor = hitInfo.transform.InverseTransformPoint(hitInfo.point);
+            _hook.connectedBody = hitInfo.rigidbody;
+        }
+    }
+
+    private void UpdateWire()
+    {
+        if(_hook.enabled)
+        {
+            Vector3[] pos = new Vector3[2];
+            pos[0] = transform.TransformPoint(_hook.anchor);
+            pos[1] = _hook.connectedBody.transform.TransformPoint(_hook.connectedAnchor);
+            _wire.SetPositions(pos);
+        }
+    } 
 }
