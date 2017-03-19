09cb542cdca29f2444c2a60a1b7482660a056a8c
Unknown <charlesarvie2@gmail.com>
2017-03-19 13:46:31 -0600

    Drill assets

---
+++ ProjectCardinalPioneers/Assets/Scripts/InteractionScripts/drillScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drillScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown("e"))
+            StartCoroutine("Drill");       
+    }
+
+    public void Drill(GameObject)
+    {
+        
+    }
+
+}
