81318cd9da662dec5b4ffa33736df661d02d282f
Unknown <charlesarvie2@gmail.com>
2017-03-19 16:47:28 -0600

    Snap to position for water converter

--- ProjectCardinalPioneers/Assets/Scripts/InteractionScripts/WaterInteraction.cs
+++ ProjectCardinalPioneers/Assets/Scripts/InteractionScripts/WaterInteraction.cs
@@ -12,18 +12,12 @@ public class WaterInteraction : MonoBehaviour
         {
             if (Vector2.Distance(other.transform.position, transform.position) < 0.5)
             {
-                //Set transform
-                other.gameObject.transform.position = (Vector2)transform.position;
-                other.gameObject.transform.rotation = transform.rotation;
+                other.transform.position = transform.position;
+                other.transform.rotation = transform.rotation;
 
-                //Freeze transform
-                Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+                Rigidbody2D rb = GetComponent<Rigidbody2D>();
                 rb.constraints = RigidbodyConstraints2D.FreezeAll;
-
-                GetComponent<Collider2D>().enabled = false;
-
-                //Activate the selection menu
-                other.transform.Find("Pod Select").gameObject.SetActive(true);
+                   
             }
         }
     }
