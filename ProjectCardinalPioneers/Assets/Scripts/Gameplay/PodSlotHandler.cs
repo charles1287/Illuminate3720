8965317c2c25b596cb3096ab261ba94241272efd
tiboedturtels <u0927597@utah.edu>
2017-03-23 00:11:39 -0600

    Started on base component snapping

--- ProjectCardinalPioneers/Assets/Scripts/Gameplay/PodSlotHandler.cs
+++ ProjectCardinalPioneers/Assets/Scripts/Gameplay/PodSlotHandler.cs
@@ -2,28 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PodSlotHandler : MonoBehaviour {
+public class PodSlotHandler : MonoBehaviour
+{
+    public bool Locked = false;
+
+    const float _gridOffset = 2f;
+    GameObject _connectedObject = null;
 
     void OnTriggerStay2D(Collider2D other)
     {
-        if(other.tag == "Pod")
+        if (other.tag == "Connection")
         {
-            if(Vector2.Distance(other.transform.position, transform.position) < 0.5)
+            if (Vector2.Distance(other.transform.position, transform.position) < 0.5)
             {
-                //Set transform
-                other.transform.parent = transform.parent;
-                other.gameObject.transform.position = (Vector2) transform.position;
-                other.gameObject.transform.rotation = transform.rotation;
-
-                //Freeze transform
-                Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-                rb.constraints = RigidbodyConstraints2D.FreezeAll;
-
-                GetComponent<Collider2D>().enabled = false;
-
-                //Activate the selection menu
-                other.transform.Find("Pod Select").gameObject.SetActive(true);
+                //This pod must be frozen and the other must be free
+                if (GetComponent<BoxCollider2D>().attachedRigidbody.constraints == RigidbodyConstraints2D.FreezeAll)
+                    if (other.attachedRigidbody.constraints == RigidbodyConstraints2D.None)
+                        SnapConnectionToThis(other);
             }
         }
+    }
+
+    void SnapConnectionToThis(Collider2D other)
+    {
+        Transform thisPod = GetComponent<BoxCollider2D>().attachedRigidbody.transform;
+        Transform otherPod = other.attachedRigidbody.transform;
+
+        //Set and lock transform on other
+        otherPod.position = (Vector2)transform.position;
+        otherPod.rotation = transform.rotation;
+        otherPod.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+
+        //Set the connected objects
+        SetConnected(other.gameObject);
+        other.GetComponent<PodSlotHandler>().SetConnected(gameObject);
+
+        //Activate the selection menu from the pod shell
+        other.transform.parent.parent.Find("Pod Select").gameObject.SetActive(true);
+    }
+
+    public void SetConnected(GameObject otherObject)
+    {
+        _connectedObject = otherObject;
     }
 }
