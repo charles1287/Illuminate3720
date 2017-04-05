695845afe43a08ccb0f39f1433a4890934852086
charles1287 <charlesarvie2@gmail.com>
2017-04-04 18:16:34 -0600

    Add files via upload
    Singleton Resource Script

---
+++ Resources.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : MonoBehaviour 
+{
+    
+    public float Water = 20f;
+    public float Ice = 20f;
+    public float Power = 20f;
+    public float Fuel = 20f;
+    public float Food = 20f;
+
+	public static Resources s_Instance = null;
+
+	public static Resources instance
+	{
+		get
+		{
+			if (s_Instance == null) 
+				s_Instance = FindObjectOfType (typeof(Resources)) as Resources;
+			
+
+			if (s_Instance == null) 
+			{
+				GameObject resource = new GameObject ("Resources");
+				s_Instance = resource.AddComponent(typeof(Resources)) as Resources;
+				Debug.Log("Could not locate a Resource object. / Resource was generated automatically.");
+			}
+
+			return s_Instance;
+		}	
+	}
+
+	void OnApplicationQuit()
+	{
+		s_Instance = null;
+	}
+}
