57c35fd6ccca4255feeff43cec1d0b833d44cfe6
charles1287 <charlesarvie2@gmail.com>
2017-04-06 16:56:09 -0600

    Add files via upload
    blargh

--- ProjectCardinalPioneers/Assets/Scripts/GameState/Resources.cs
+++ ProjectCardinalPioneers/Assets/Scripts/GameState/Resources.cs
@@ -2,19 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Resources : MonoBehaviour 
+public class Resources : MonoBehaviour
 {
-    
-	public static float Water = 20f;
-	public static float Ice = 20f;
-    public static float Power = 20f;
-    public static float Fuel = 20f;
-    public static float Food = 20f;
+	public float Water = 0f;
+	public float Power = 0f;
+	public float Food = 0f;
+	public float Ice = 0f;
+	public float Fuel = 0f;
+    		
+	public static Resources s_Instance = null;
 
-	public float waterCount = Water;
-	public float iceCount = Ice;
-	public float powerCount = Power;
-	public float fuelCount = Fuel;
-	public float foodCount = Food;
+	public static Resources instance {
+		
+		get {
+			if (s_Instance == null)
+				s_Instance = FindObjectOfType (typeof(Resources)) as Resources;
+							
 
+			if (s_Instance == null) {
+				GameObject resource = new GameObject ("Resources");
+				s_Instance = resource.AddComponent (typeof(Resources)) as Resources;
+				Debug.Log ("Could not locate a Resource object. / Resource was generated automatically.");
+			}
+
+			return s_Instance;
+		}	
+	}
+
+	void OnApplicationQuit ()
+	{
+		s_Instance = null;
+	}
 }
+
