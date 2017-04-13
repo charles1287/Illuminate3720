e698e7a5c5a70d4dd7cfea18c449f447b28dd653
charles1287 <charlesarvie2@gmail.com>
2017-04-12 18:28:45 -0600

    Add files via upload
    Pod counters

--- ProjectCardinalPioneers/Assets/Scripts/Pod Type Linking/PodLinking.cs
+++ ProjectCardinalPioneers/Assets/Scripts/Pod Type Linking/PodLinking.cs
@@ -8,21 +8,31 @@ public class PodLinking : MonoBehaviour
     public GameObject WaterPodPrefab;
     public GameObject PowerPodPrefab;
 
+	public float airRate = 2f;
+	public float waterRate = 2f;
+	public float powerRate = 2f;
+
     public void MakeAirPod(GameObject selectMenu)
     {
         Instantiate(AirPodPrefab, transform, false);
         selectMenu.SetActive(false);
+
+		AirPodPrefab += 1;
     }
 
     public void MakePowerPod(GameObject selectMenu)
     {
         Instantiate(PowerPodPrefab, transform, false);
         selectMenu.SetActive(false);
+
+		PowerPodPrefab += 1;
     }
 
     public void MakeWaterPod(GameObject selectMenu)
     {
         Instantiate(WaterPodPrefab, transform, false);
         selectMenu.SetActive(false);
+
+		WaterPodPrefab += 1;
     }
 }
